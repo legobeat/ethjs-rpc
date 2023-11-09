2ca43cb1709002616ef5e82435179820d4086466
legobt <6wbvkn0j@anonaddy.me>
2023-11-09 02:03:26 +0000

    devDeps: eslint@2->^5
    - disable some new rules
    - apply lint fix

--- src/tests/test.index.js
+++ src/tests/test.index.js
@@ -1,7 +1,8 @@
-const EthRPC = require('../index.js');
-const assert = require('chai').assert;
+const { assert } = require('chai');
 const TestRPC = require('ganache-cli');
 const HTTPProvider = require('@metamask/ethjs-provider-http');
+const EthRPC = require('../index.js');
+
 const provider = TestRPC.provider({});
 const provider2 = TestRPC.provider({});
 
@@ -32,10 +33,10 @@ describe('ethjs-rpc', () => {
           assert.notDeepEqual(accounts1, accounts2);
 
           eth.sendAsync({ method: 'eth_accounts' })
-          .then((accounts3) => {
-            assert.deepEqual(accounts3, accounts2);
-            done();
-          })
+            .then((accounts3) => {
+              assert.deepEqual(accounts3, accounts2);
+              done();
+            })
           .catch((error) => console.log(error)); // eslint-disable-line
         });
       });
@@ -58,9 +59,11 @@ describe('ethjs-rpc', () => {
     });
 
     it('should handle invalid response', (done) => {
-      const eth = new EthRPC({ sendAsync: (payload, cb) => {
-        cb(null, { error: 'Some Error!!' });
-      } });
+      const eth = new EthRPC({
+        sendAsync: (payload, cb) => {
+          cb(null, { error: 'Some Error!!' });
+        },
+      });
       eth.sendAsync({ method: 'eth_accounts' }, (err, accounts1) => {
         assert.equal(typeof err, 'object');
         assert.equal(accounts1, null);
@@ -89,9 +92,11 @@ describe('ethjs-rpc', () => {
     });
 
     it('should handle invalid errors', (done) => {
-      const eth = new EthRPC({ sendAsync: (payload, cb) => {
-        cb('Some error!');
-      } });
+      const eth = new EthRPC({
+        sendAsync: (payload, cb) => {
+          cb('Some error!');
+        },
+      });
       eth.sendAsync({ method: 'eth_accounts' }, (err, accounts1) => {
         assert.equal(typeof err, 'object');
         assert.equal(accounts1, null);
